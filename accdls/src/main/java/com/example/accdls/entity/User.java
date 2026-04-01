748e1b362a4830a8b7d125bd937ba81023a82863
trantrihuu2006 <on2318800@gmail.com>
2026-04-01 16:02:08 +0700

    Add files via upload

---
+++ accdls/src/main/java/com/example/accdls/entity/User.java
@@ -0,0 +1,28 @@
+package com.example.accdls.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+    String username;
+    String password;
+    String firstname;
+    String lastname;
+    LocalDate born;
+}
